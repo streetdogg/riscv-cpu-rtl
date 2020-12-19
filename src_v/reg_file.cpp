5bb46b0f854f0f752971bf912e4688d3aa4def05
Piyush Itankar <pitankar@gmail.com>
2020-12-19 22:12:44 +0530

    Adds register file module tests
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- src_v/reg_file.cpp
+++ src_v/reg_file.cpp
@@ -10,10 +10,50 @@ using namespace std;
 int main(int argc, char **argv, char **env) {
     Verilated::commandArgs(argc, argv);
     Vreg_file *reg_file = new Vreg_file;
-    int status = 0;
-   
-    printf("TODO: Implement reg_file module tests\n");
+    int status = 0, reg = 1;
 
+    printf("Writing to the Registers\n");
+    reg_file->w_en = true;
+    for (reg = 1; reg < 32; reg++) {
+        reg_file->rda = reg;
+        reg_file->rd = reg * 2;
+
+        reg_file->eval();
+    }
+    reg_file->w_en = false;
+
+    printf("Checking Written values by probing rs1: ");
+    for (reg = 1; reg < 32; reg++) {
+        reg_file->rs1a = reg;
+        reg_file->eval();
+
+        ASSERT((reg_file->rs1 == reg*2), status, terminate, "FAILED! [reg %02d]\n", reg);
+    }
+    printf("PASSED!\n");
+
+    printf("Checking Written values by probing rs2 with read disabled: ");
+    reg_file->rs2val = false;
+    for (reg = 1; reg < 32; reg++) {
+        reg_file->rs2a = reg;
+        reg_file->eval();
+
+        ASSERT((reg_file->rs2 == 0), status, terminate, "FAILED! [reg %02d]\n", reg);
+    }
+    printf("PASSED!\n");
+
+    printf("Checking Written values by probing rs2 with read enabled: ");
+    reg_file->rs2val = true;
+    for (reg = 1; reg < 32; reg++) {
+        reg_file->rs2a = reg;
+        reg_file->eval();
+
+        ASSERT((reg_file->rs2 == reg*2), status, terminate, "FAILED! [reg %02d]\n", reg);
+    }
+    reg_file->rs2val = false;
+    printf("PASSED!\n");
+
+terminate:
+    reg_file->final();
     delete reg_file;
     return status;
 }
