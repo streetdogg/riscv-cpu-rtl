0b79882e0d7353f1b15da8329f2985c3eec560c7
Piyush Itankar <pitankar@gmail.com>
2020-12-19 22:28:49 +0530

    Adds control unit module and test template
    Also updates the copyright headers in all the verification related
    sources.
    
    Signed-off-by: Piyush Itankar <pitankar@gmail.com>

--- src/ctrl_unit.sv
+++ src/ctrl_unit.sv
@@ -22,50 +22,6 @@
  * SOFTWARE.
  */
 
-#include <fstream>
-#include <iostream>
+module ctrl_unit();
 
-#include "test_infra.h"
-#include "Vinstmem.h"
-#include "verilated.h"
-
-using namespace std;
-
-int main(int argc, char **argv, char **env) {
-    Verilated::commandArgs(argc, argv);
-    Vinstmem *instmem = new Vinstmem;
-    
-    int status = 0, address = 0, instruction;
-    string line;
-    ifstream progFile("program.txt");
-
-    while (!progFile.eof()) {
-        instmem->addr = address;
-        instmem->eval();
-
-        // Read a line from the program file
-        getline(progFile, line);
-        if (line.length() == 0)
-            break;
-
-        // Convert to hex if the line was no empty
-        instruction = stoi(line, 0, 16);
-
-        // Check if the memory has the same content as the file
-        ASSERT((instmem->instr == instruction),
-               status,
-               terminate,
-               "[%03d]: 0x%04x != 0x%04x \n", address, instmem->instr, instruction);
-
-        address++;
-    }
-
-    printf("All Checks Passed!\n");
-
-terminate:
-    progFile.close();
-    instmem->final();
-    delete instmem;
-
-    return status;
-}
+endmodule
--- src_v/ctrl_unit.cpp
+++ src_v/ctrl_unit.cpp
@@ -26,46 +26,18 @@
 #include <iostream>
 
 #include "test_infra.h"
-#include "Vinstmem.h"
+#include "Vctrl_unit.h"
 #include "verilated.h"
 
 using namespace std;
 
 int main(int argc, char **argv, char **env) {
     Verilated::commandArgs(argc, argv);
-    Vinstmem *instmem = new Vinstmem;
+    Vctrl_unit *ctrl_unit = new Vctrl_unit;
+    int status = 0;
     
-    int status = 0, address = 0, instruction;
-    string line;
-    ifstream progFile("program.txt");
+    printf("TODO: Implement the control unit tests\n");
 
-    while (!progFile.eof()) {
-        instmem->addr = address;
-        instmem->eval();
-
-        // Read a line from the program file
-        getline(progFile, line);
-        if (line.length() == 0)
-            break;
-
-        // Convert to hex if the line was no empty
-        instruction = stoi(line, 0, 16);
-
-        // Check if the memory has the same content as the file
-        ASSERT((instmem->instr == instruction),
-               status,
-               terminate,
-               "[%03d]: 0x%04x != 0x%04x \n", address, instmem->instr, instruction);
-
-        address++;
-    }
-
-    printf("All Checks Passed!\n");
-
-terminate:
-    progFile.close();
-    instmem->final();
-    delete instmem;
-
+    delete ctrl_unit;
     return status;
 }
